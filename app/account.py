302c0f5c2bdfa4a763af8483b097628c0bdeee7b
LeoHo <leoho@msi.com>
2024-08-29 11:14:45 +0800

    Improve Neopass login rules.

--- app/account.py
+++ app/account.py
@@ -1,13 +1,12 @@
-import asyncio
 import json
+from random import randrange
 from playwright.async_api import Page, BrowserContext
 from os.path import exists
 from pathlib import Path
 import urls.neopets_urls as NEOPETS_URLS
-from utility import random_sleep
+from utility import random_sleep, web
 
 class NotLoggedInException(Exception):
-    "Raised when we cannot confirm we've logged in successfully."
     pass
 
 class Account:
@@ -29,7 +28,6 @@ class Account:
     async def logout(self, context: BrowserContext, page: Page) -> None:
         await page.goto(NEOPETS_URLS.NEO_LOGOUT_REQUEST)
         print(f'{self._username} logout...')
-        self._remove_cookie()
         await random_sleep(5, 10)
 
     async def login(self, context: BrowserContext, page: Page) -> None:
@@ -47,6 +45,7 @@ class Account:
                 return True
             except Exception as e:
                 print(f"{self._username} Login error {e}")
+                await self.logout(context, page)
             
             await random_sleep(10, 240)
             count += 1
@@ -55,33 +54,33 @@ class Account:
         
     async def _login_or_restore_cookie(self, context: BrowserContext, page: Page):
         '''
-        登入帳號，如果Cookie 存在，就以Cookie 操作。
+        "Log in to the account. If cookies exist, use the cookies instead."
         '''
         _username_t = self._username if self._legacy else self._neopass_username
         cookie_path = f"sessions/{_username_t}.json"
 
         if exists(cookie_path):
             await context.add_cookies(json.loads(Path(cookie_path).read_text()))
-            print("Loading cookie file.")
-            await page.goto(NEOPETS_URLS.NEO_INVENTORY_R)
+            print(f"{_username_t} Loading cookie file.")
+            await page.goto(NEOPETS_URLS.NEO_BANK, wait_until="load", timeout=60000)
             await random_sleep(5, 10)
 
             if not await  self._confirm_manual_login(page):
                 self._remove_cookie()
                 await self._login_account_portal(context, page)
                 if await self._confirm_manual_login(page):
-                    print("Login Success.")
+                    print(f"{_username_t} Login Success.")
                     await self._store_cookie(context, page)
                 else:
-                    raise NotLoggedInException
-            print("Login Success using Cookie.")
+                    raise NotLoggedInException("Raised when we cannot confirm we've logged in successfully.")
+            print(f"{_username_t} Login Success using Cookie.")
         else:
             await self._login_account_portal(context, page)
             if await self._confirm_manual_login(page):
-                print("Login Success.")
+                print(f"{_username_t} Login Success.")
                 await self._store_cookie(context, page)
             else:
-                raise NotLoggedInException
+                raise NotLoggedInException("Raised when we cannot confirm we've logged in successfully.")
             
     async def _login_account_portal(self, context: BrowserContext, page: Page):
         if self._legacy:
@@ -91,44 +90,75 @@ class Account:
             
     async def _login_account_neopass(self, context: BrowserContext, page: Page):
         '''
-        2022加入的NeoPass 帳戶登入
+        Login with the NeoPass account added in 2022.
         '''
+        count = 0
         await page.goto(NEOPETS_URLS.NEO_NEOPASS_LOGIN)
         await random_sleep(10, 15)
-        try:
-            await page.locator("input[name=\"email\"]").click()
-            await page.locator("input[name=\"email\"]").fill(self._username)
-            await random_sleep(2, 5)
-            await page.locator("input[name=\"password\"]").click()
-            await page.locator("input[name=\"password\"]").fill(self._password)
-            await random_sleep(2, 5)
-            await page.get_by_role("button", name="Sign In").click()
-        except:
-            # cache with cookie.
-            pass
-        await random_sleep(10, 15)
-        await page.goto("https://account.neopets.com/classic/login")
-        await random_sleep(10, 15)
-        await page.locator(f"text={self._neopass_username}").click()
+        while count < 5:
+            try:
+                # Avoid reChatcha v3 
+                email_field_box = await page.locator("input[name=\"email\"]").bounding_box()
+                if email_field_box:
+                    await page.mouse.click(email_field_box['x'] + randrange(1, 50), email_field_box['y'] + randrange(1,10))
+                    await page.locator("input[name=\"email\"]").fill(self._username)
+                    
+                await random_sleep(2, 5)
+                
+                password_field_box = await page.locator("input[name=\"password\"]").bounding_box()
+                if password_field_box:
+                    await page.mouse.click(password_field_box['x'] + randrange(1, 50), password_field_box['y'] + randrange(1,10))
+                    await page.locator("input[name=\"password\"]").fill(self._password)
+                    
+                await random_sleep(2, 5)
+                await page.keyboard.press('Tab')
+                await random_sleep(1,2)
+                await page.keyboard.press('Tab')
+                await random_sleep(1,2)
+                await page.keyboard.press('Enter')
+
+                await page.wait_for_load_state('networkidle')
+
+            except Exception as e:
+                print(f"{__name__}: {e}")
+                pass
+            await random_sleep(10, 15)
+            await page.goto("https://account.neopets.com/classic/login")
+            await random_sleep(10, 15)
+
+            if NEOPETS_URLS.NEO_NEOPASS_LOGIN in page.url:
+                count += 1
+            else:
+                break
+
+        account_field_box = await page.locator(f"text={self._neopass_username}").bounding_box()
+        if account_field_box:
+            await page.mouse.click(account_field_box['x'] + randrange(1, 50), account_field_box['y'] + randrange(1,10))
+
         await page.get_by_role("button", name="Continue").click()
         await random_sleep(10, 15)
 
     async def _login_account_legacy(self, context: BrowserContext, page: Page):
         '''
-        傳統的Neopets 帳戶登入
+        Login with the traditional Neopets account.
         '''
         await page.goto(NEOPETS_URLS.NEO_LOGIN)
-        await random_sleep(30, 40)
-        await page.locator("#loginUsername").click()
-        await page.locator("#loginUsername").fill(self._username)
-        await page.locator("#loginPassword").click()
-        await page.locator("#loginPassword").fill(self._password)
-        await page.get_by_role("button", name="Log In").click()
-        await random_sleep(30, 40)
+        retry = 0
+        while retry < 5:
+            await random_sleep(30, 40)
+            payload = {"destination": "", "username": self._username, "password": self._password, }
+            rep = await web.post(payload, NEOPETS_URLS.NEO_LOGIN_REQUEST, context, page, NEOPETS_URLS.NEO_LOGIN)
+
+            if web.check_for_announcement(rep):
+                retry += 1
+                print(f"{__name__} retry...")
+            else:
+                break
+        await random_sleep(5, 10)
 
     async def _store_cookie(self, context: BrowserContext, page: Page):
         '''
-        儲存Cookie
+        Store cookie
         '''
         cookies = await context.cookies()
         _username_t = self._username if self._legacy else self._neopass_username
@@ -152,8 +182,9 @@ class Account:
 
     async def _confirm_manual_login(self, page: Page) -> bool:
         '''
-        確認是否登入中
+        Check if logged in.
         '''
+        await page.goto(NEOPETS_URLS.NEO_BANK, wait_until="load", timeout=60000)
         content = await page.content()
         if self._legacy:
             if f'userlookup.phtml?user={self._username}' in content:
