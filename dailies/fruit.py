ccbefe54a25e7e1d02f44a460135128eea5f9d2a
LeoHo <leoho@msi.com>
2024-09-11 16:04:11 +0800

    Fixed fruit condition.

--- dailies/fruit.py
+++ dailies/fruit.py
@@ -3,15 +3,20 @@ import urls.neopets_urls as NEOPETS_URLS
 from utility import random_sleep
 
 async def get(context: BrowserContext, page: Page) -> bool:
+    flag = False
     await random_sleep()
     try:
         _page = await context.new_page()
         await _page.goto(NEOPETS_URLS.NEO_FRUIT)
-        await _page.get_by_role("button", name="Spin, spin, spin!").click()
+        _content = await _page.content()
+        if "Please come back tomorrow and try again" not in _content:
+            await _page.get_by_role("button", name="Spin, spin, spin!").click()
+        
+        flag = True
         await random_sleep()
         await _page.close()
-        return True
+        return flag
     except Exception as e:
         print("get_friut complete")
 
-    return False
+    return flag
